bcdfa6826a41f3b49444a8d49730aecb6c01a830
Elias Farhan <elias.farhan@gmail.com>
2024-05-31 18:49:24 +0200

    Continue to work on vec2

--- Core/include/math/vec2.h
+++ Core/include/math/vec2.h
@@ -6,10 +6,11 @@
 
 namespace neko {
 template<typename T>
-class Vec2 {
+class Vec2
+{
 public:
     T x{}, y{};
-    Vec2<T>(){}
+    Vec2<T>()= default;
     Vec2<T>(T x, T y): x(x), y(y){}
 
     template<class U>
@@ -18,6 +19,13 @@ public:
     [[nodiscard]] constexpr Vec2<T> operator+(Vec2<T> other) const
     {
         return {x + other.x, y + other.y};
+    }
+
+    constexpr Vec2<T>& operator+=(Vec2<T> other)
+    {
+        x += other.x;
+        y += other.y;
+        return *this;
     }
 
     [[nodiscard]] constexpr Vec2<T> operator-(Vec2<T> other) const
@@ -30,6 +38,13 @@ public:
         return {-x, -y};
     }
 
+    constexpr Vec2<T>& operator-=(Vec2<T> other)
+    {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
     [[nodiscard]] constexpr Vec2<T> operator*(T other) const
     {
         return {x * other, y * other};
@@ -40,9 +55,23 @@ public:
         return {x * other.x, y * other.y};
     }
 
+    constexpr Vec2<T>& operator*=(T other)
+    {
+        x *= other;
+        y *= other;
+        return *this;
+    }
+
     [[nodiscard]] constexpr Vec2<T> operator/(T other) const
     {
         return {x / other, y / other};
+    }
+
+    constexpr Vec2<T>& operator/=(T other)
+    {
+        x /= other;
+        y /= other;
+        return *this;
     }
 
     [[nodiscard]] constexpr static T Dot(Vec2<T> v1, Vec2<T> v2)
@@ -55,16 +84,24 @@ public:
         return Dot(*this, *this);
     }
 
-    [[nodiscard]] T Length() const requires std::is_floating_point_v<T>
+    [[nodiscard]] T Length() const
+        requires std::is_floating_point_v<T>
     {
         return std::sqrt(Dot(*this, *this));
     }
 
-    [[nodiscard]] constexpr static T Lerp(Vec2<T> v1, Vec2<T> v2, T t)requires std::is_floating_point_v<T>
+    [[nodiscard]] constexpr static T Lerp(Vec2<T> v1, Vec2<T> v2, T t)
+        requires std::is_floating_point_v<T>
     {
         return v1 + (v2 - v1) * t;
     }
 };
+
+template<typename T>
+Vec2<T> operator*(T value, Vec2<T> vector)
+{
+    return {value*vector.x, value*vector.y};
+}
 
 using Vec2f = Vec2<float>;
 using Vec2d = Vec2<double>;
--- Core/test/maths/test_vec2.cpp
+++ Core/test/maths/test_vec2.cpp
@@ -11,11 +11,29 @@ TEST_P(Vec2fFixture, Add)
     const auto result = v1+v2;
     EXPECT_FLOAT_EQ(result.x, v1.x+v2.x);
     EXPECT_FLOAT_EQ(result.y, v1.y+v2.y);
+
+    v2 += v1;
+    EXPECT_FLOAT_EQ(v2.x, result.x);
+    EXPECT_FLOAT_EQ(v2.y, result.y);
+}
+
+TEST_P(Vec2fFixture, Sub)
+{
+    auto [v1, v2] = GetParam();
+    const auto result = v1-v2;
+    EXPECT_FLOAT_EQ(result.x, v1.x+v2.x);
+    EXPECT_FLOAT_EQ(result.y, v1.y+v2.y);
+
+    v2 -= v1;
+    EXPECT_FLOAT_EQ(v2.x, result.x);
+    EXPECT_FLOAT_EQ(v2.y, result.y);
 }
 
 INSTANTIATE_TEST_SUITE_P(AllNumbers,
                          Vec2fFixture,
                          testing::Values(
-                                 std::pair<neko::Vec2f, neko::Vec2f>{ {0.0f, 0.0f}, {1.2f,-3.4f} }
+                                 std::pair<neko::Vec2f, neko::Vec2f>{ {0.0f, 0.0f}, {1.2f,-3.4f} },
+                                 std::pair<neko::Vec2f, neko::Vec2f>{ {-3.0f, 0.0f}, {1.2f,-3.4f} },
+                                 std::pair<neko::Vec2f, neko::Vec2f>{ {0.0f, 1.5f}, {1.2f,-3.4f} }
                          )
 );
